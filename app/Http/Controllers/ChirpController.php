04a7e9f8f9125677cc63a97c1cfa37b0c003e40f
lino <linox_2@hotmail.com>
2022-11-24 19:25:45 -0300

    Criação da política de modelo e a edição e delete

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -18,6 +18,7 @@ class ChirpController extends Controller
         // return 'Olá Mundo';
         return Inertia::render('Chirps/Index', [
             //
+            'chirps' => Chirp::with('user:id,name')->latest()->get(),
         ]);
     }
 
@@ -79,7 +80,15 @@ class ChirpController extends Controller
      */
     public function update(Request $request, Chirp $chirp)
     {
-        //
+        $this->authorize('update', $chirp);
+ 
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+ 
+        $chirp->update($validated);
+ 
+        return redirect(route('chirps.index'));
     }
 
     /**
@@ -90,6 +99,10 @@ class ChirpController extends Controller
      */
     public function destroy(Chirp $chirp)
     {
-        //
+        $this->authorize('delete', $chirp);
+ 
+        $chirp->delete();
+ 
+        return redirect(route('chirps.index'));
     }
 }
--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -6,7 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Chirp extends Model
-{
+{   
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
     use HasFactory;
 
     protected $fillable = [
