89ac0881d486999fef17d412a8a87e0d385ea6ea
lino <linox_2@hotmail.com>
2022-11-24 19:40:15 -0300

    Criação das notificações e eventos

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Events\ChirpCreated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,10 +12,14 @@ class Chirp extends Model
     {
         return $this->belongsTo(User::class);
     }
-    
+
     use HasFactory;
 
     protected $fillable = [
         'message',
+    ];
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
     ];
 }
